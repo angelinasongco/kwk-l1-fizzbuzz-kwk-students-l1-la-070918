8a9b2eb2e21fa1b0a6dc683d8ff0661176d0765c
angelinasongco <angelinasongco123@gmail.com>
2018-07-10 21:20:56 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+
+def fizzbuzz(x)
+  if x%5 == 0 && x%3 == 0  
+   "FizzBuzz"
+  elsif x%5 == 0
+    "Buzz"
+  elsif x%3 == 0
+    "Fizz"
+  end
+end
+
+fizzbuzz(4)
+fizzbuzz(9)
+fizzbuzz(5)
+fizzbuzz(15)
+
